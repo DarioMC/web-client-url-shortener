8ee31126053244bf1b78573d8787664d763bca17
Unknown <dmcsubotai95@gmail.com>
2019-06-23 23:07:53 -0600

    Component create

---
+++ src/components/Create.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+
+
+class Create extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { 
+            tops: []
+        };
+    }
+    
+    componentDidMount() {        
+        fetch('https://urlshortened.herokuapp.com/date.json', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then((response) => { return response.json(); })
+            .then((data) => {
+                this.setState({ tops: data['data'] });
+            })
+    }
+    
+    render() {
+        if(this.props.print_create){
+            if(this.state.tops.length > 0) {
+                const urls = this.state.tops.map((url, i) => {
+                    return (
+                        <section className="tiles" key={ url['id'] }>
+                            <article className="style1">
+                                <a href={ url['short_url'] }>
+                                    <h4>{ url['title'] }</h4>
+                                </a>
+                                <div className="content">
+                                    <p>Original Url: { url['original_url'] }</p>
+                                    <p>Short Url: { url['short_url'] }</p>
+                                    <p>Visit Count: { url['visit_count'] }</p>
+                                </div>
+                            </article>
+                        </section>
+                    )
+                });
+
+                return (
+                    <div id="main">
+                        <div className="Create">
+                            <div className="inner">
+                                { urls }
+                            </div>
+                        </div>
+                    </div>
+                )
+            } else {
+                return (
+                    <p className="text-center">Loding...</p>
+                )
+            }
+        } else {
+            return(
+                <p className="text-center"></p>
+            )
+        }
+    }   
+}
+
+export default Create;
